888abebd6e5c5a71d0aebcbb671e934bfd0ef624
rasheed1998 <rasheedabdul1998@gmail.com>
2021-09-20 14:59:31 +0530

    first file

---
+++ 1.js
@@ -0,0 +1,20 @@
+const countries = [
+    'ALBANIA',
+    'BOLIVIA',
+    'CANADA',
+    'DENMARK',
+    'ETHIOPIA',
+    'FINLAND',
+    'GERMANY',
+    'HUNGARY',
+    'IRELAND',
+    'JAPAN',
+    'KENYA'
+  ]
+for (let i=0; i < countries.length;i++)
+{
+    const len =countries[i].length;
+    const code = countries[i].slice(0,3);
+    const city =countries[i];
+    console.log([city.code,len])
+}  
